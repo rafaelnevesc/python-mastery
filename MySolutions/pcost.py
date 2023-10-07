14d3e9376463f81a671466187df11aa2ad72e1e1
rafaelnevesc <rafaael.neves@gmail.com>
2023-10-07 20:12:35 -0300

    edit: ex 1.4 a

--- MySolutions/pcost.py
+++ MySolutions/pcost.py
@@ -1,10 +1,12 @@
-total_cost = 0.0
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(f'../Data/{filename}', 'r') as f:
+        for line in f:
+            contents = line.split()
+            shares = int(contents[1])
+            price = float(contents[2])
+            total_cost = total_cost + (shares * price)
+    return total_cost
 
-with open('../Data/portfolio.dat', 'r') as f:
-    for line in f:
-        contents = line.split()
-        shares = int(contents[1])
-        price = float(contents[2])
-        total_cost = total_cost + (shares * price)
 
-print(total_cost)
+portfolio_cost('portfolio.dat')
